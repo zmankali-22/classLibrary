2ed70338517d6374ce881551fca81c3bbc3347bd
utsav <sav.acharya22@gmail.com>
2024-05-04 09:49:54 +1000

    feat: add books to library

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 
 //  Import library class, books class, audiobook class, movies class, music class
 
+const { Book } = require("./Book");
 const { Media } = require("./Media");
+const { Library } = require("./library");
 
 
 // create instances of classes
 
-let lotr = new Media("fellowShip of the ring")
+let lotr = new Book("fellowShip of the ring")
 console.log(lotr)
 console.log(lotr.name)
 
@@ -20,3 +22,10 @@ console.log(lotr.name)
 // create an instance of a library
 
 // add media instances into library instance
+
+let libraryInstance = new Library()
+libraryInstance.books.push(lotr)
+
+// log the data in the library
+console.log(libraryInstance)
+console.log(libraryInstance.books[0].name)
