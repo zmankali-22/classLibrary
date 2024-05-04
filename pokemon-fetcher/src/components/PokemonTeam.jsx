57813cf83c17abca576f6d1d9d535ad17895a3ee
utsav <sav.acharya22@gmail.com>
2024-05-04 10:43:05 +1000

    feat: component mount on state

--- pokemon-fetcher/src/components/PokemonTeam.jsx
+++ pokemon-fetcher/src/components/PokemonTeam.jsx
@@ -2,25 +2,37 @@ import React from 'react';
 
 
 export default class PokemonTeam extends React.Component {
-
- 
+        constructor(props) {
+            super(props);
+            this.state = {
+                PokemonTeam: []
+            }
+        }
 
     //  get random pokemon number
 
-     getRandomPokemonId() {
-        return Math.floor(Math.random() * 1025) + 1;
-    }
+  
 
     async componentDidMount() {
-        let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + this.getRandomPokemonId()).catch(error => error);
+
+
+      function  getRandomPokemonId() {
+            return Math.floor(Math.random() * 1025) + 1;
+        }
+
+        let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + getRandomPokemonId()).catch(error => error);
         let data = await response.json();
         console.log(data.name);
+        this.setState({PokemonTeam: [...this.state.PokemonTeam, data] })
     }
 
     render() {
         return (
             <div>
                 <h1>Pokemon data here</h1>
+                {this.state.PokemonTeam && this.state.PokemonTeam.map(pokemon => {
+                    return <h1 key={pokemon.name} >{pokemon.name}</h1>
+                })}
             </div>
         );
     }
